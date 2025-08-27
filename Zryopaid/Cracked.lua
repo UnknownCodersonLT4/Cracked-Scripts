005e55fedb212dee1021ba73e4d77eea57e83223
SlayersonV4 <coderobierika@gmail.com>
2025-08-26 21:05:35 -0400

    Update Cracked.lua

--- Zryopaid/Cracked.lua
+++ Zryopaid/Cracked.lua
@@ -1,6 +1,3 @@
-local function loadScripts()
-    loadstring(game:HttpGet("https://pastefy.app/1iuaZFDO/raw", true))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/UnknownCodersonLT4/Cracked-Scripts/main/NovaPaidV2/src/Cracked.lua", true))()
-end
 
-loadScripts()
+    loadstring(game:HttpGet("https://pastefy.app/1iuaZFDO/raw", true))()
+    
