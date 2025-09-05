9aec79ca18f5d9174a7216a16ae6303b209d9720
SlayersonV4 <coderobierika@gmail.com>
2025-09-04 23:56:33 -0400

    Update and rename saves to saves.lua

--- Zryopaid/saves.lua
+++ Zryopaid/saves.lua
@@ -7,3 +7,6 @@
 
 --https://raw.githubusercontent.com/Bad031/Green-Eye/refs/heads/main/Gec.Forest
 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UnknownCodersonLT4/Cracked-Scripts/refs/heads/main/Zryopaid/Cracked.lua", true))()
+
